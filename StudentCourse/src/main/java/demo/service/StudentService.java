3c94840b6affa19f56378eb2413d3bba734bce09
HungLD <leduyhung102@gmail.com>
2019-05-30 11:58:39 +0700

    update

--- StudentCourse/src/main/java/demo/service/StudentService.java
+++ StudentCourse/src/main/java/demo/service/StudentService.java
@@ -1,6 +1,5 @@
 package demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -8,8 +7,6 @@ import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import demo.entity.Enrolment;
 import demo.entity.Student;
@@ -45,44 +42,14 @@ public class StudentService {
 	
 	
 	// Tìm tất cả các khóa học(thông tin chi tiết) của sinh  viên 
-	public List<Enrolment> findAllCourseOfStudent(int id) {
-		String query = "SELECT temp.*, c.name, c.fee FROM " + 
+	public List findAllCourseOfStudent(int id) {
+		String query = "SELECT temp.student_id, c.* FROM " + 
 				"(SELECT * FROM student_course.enrolment where student_id = " + id + ") as temp " + 
 				"LEFT JOIN student_course.course as c ON temp.course_id = c.course_id ";
 		
 		Query q = entityManager.createNativeQuery(query);
 		
 		return q.getResultList();
-	}
-	
-	// Đăng ký các khóa học cho sinh viên, thông tin chi tiết lưu trong List<Enrolment>
-	public void registerCourseForStudent(List<Enrolment> listEnrolment, int studentId) {
-		for(Enrolment enrolment : listEnrolment) {
-			String query = createQueryInsertEnrolment(studentId, enrolment.getCourse().getCourseId(), 
-					enrolment.getStartDate(), enrolment.getEndDate());
-			
-			Query q = entityManager.createNativeQuery(query);
-			q.executeUpdate();
-		}
-	}
-	
-	// Thêm dữ liệu vào bảng Enrolment - thêm các bản ghi trong mối quan hệ student - course (số lượng lớn)
-	public void insertDataIntoEnrolment(List<Enrolment> listEnrolment) {
-		for(Enrolment enrolment : listEnrolment) {
-			String query = createQueryInsertEnrolment(enrolment.getStudent().getStudentId() , enrolment.getCourse().getCourseId(), 
-					enrolment.getStartDate(), enrolment.getEndDate());
-			
-			Query q = entityManager.createNativeQuery(query);
-			
-			q.executeUpdate();
-		}
-	}
-	
-	
-	public String createQueryInsertEnrolment(int studentId, int courseId, String startDate, String endDate) {
-		String query = "INSERT INTO student_course.enrolment (student_id, course_id, start_date, end_date) "
-				+ "VALUE ('" + studentId + "','" + courseId +"','" + startDate + "','" + endDate + "')";
-		return query;
 	}
 	
 }
